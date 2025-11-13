4186ffb7a5e1c36f4551821939b5653912680a35
Aiym1703 <aiympangereye1703@gmail.com>
2025-11-13 14:11:26 +0500

    Add core package (UnionFind)

---
+++ src/main/java/org/example/core/UnionFind.java
@@ -0,0 +1,25 @@
+package org.example.core;
+
+public class UnionFind {
+    int[] parent, rank;
+
+    public UnionFind(int n){
+        parent=new int[n];
+        rank=new int[n];
+        for(int i=0;i<n;i++) parent[i]=i;
+    }
+
+    public int find(int x){
+        if(parent[x]!=x) parent[x]=find(parent[x]);
+        return parent[x];
+    }
+
+    public boolean union(int a,int b){
+        int pa=find(a), pb=find(b);
+        if(pa==pb) return false;
+        if(rank[pa]<rank[pb]) parent[pa]=pb;
+        else if(rank[pb]<rank[pa]) parent[pb]=pa;
+        else { parent[pb]=pa; rank[pa]++; }
+        return true;
+    }
+}
